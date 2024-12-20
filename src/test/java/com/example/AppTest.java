3e14336a16def050d2f317b44e0d53380d9bebf2
toharikr <53942441+toharikr@users.noreply.github.com>
2024-12-20 10:33:22 -0500

    Update AppTest.java

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -1,1 +1,13 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+    @Test
+    void testMain() {
+        assertTrue(true, "Sample test for App");
+    }
+}
 
